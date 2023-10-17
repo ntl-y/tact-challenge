6e1acf40464e28e8bd2a319c0a1f08af614cd675
ntl-y <n-danilkina@list.ru>
2023-10-17 05:49:40 +0300

    send

--- tests/Task4.spec.ts
+++ tests/Task4.spec.ts
@@ -1,35 +1,55 @@
-import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-import { toNano } from 'ton-core';
-import { Task4 } from '../wrappers/Task4';
+import { TonClient } from "@tonclient/core";
+import { Account } from "@tonclient/appkit";
+import { deploy, callThroughMultisig } from "@tonclient/appkit";
+import { toNano, encodeMessage, deployWithGiver, expect } from "./utils";
+import { Task4 } from "../contracts";
 
-describe('Task4', () => {
-    let blockchain: Blockchain;
-    let task4: SandboxContract<Task4>;
+describe("Task4", () => {
+  let client: TonClient;
+  let owner: Account;
+  let task4: Task4;
 
-    beforeEach(async () => {
-        blockchain = await Blockchain.create();
-        task4 = blockchain.openContract(await Task4.fromInit());
-        const deployer = await blockchain.treasury('deployer');
-        const deployResult = await task4.send(
-            deployer.getSender(),
-            {
-                value: toNano('0.05'),
-            },
-            {
-                $$type: 'Deploy',
-                queryId: 0n,
-            }
-        );
-        expect(deployResult.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: task4.address,
-            deploy: true,
-            success: true,
-        });
+  before(async () => {
+    client = new TonClient({ network: { server_address: "http://localhost" } });
+  });
+
+  it("deploys Task4 contract", async () => {
+    const code = await client.boc.get_code_from_tvc({ tvc: Task4.tvc });
+
+    owner = await Account.with({
+      client,
+      signer: { type: "None" },
+      balance: toNano(1),
     });
 
-    it('test', async () => {
+    task4 = await deployWithGiver(owner, Task4, { code });
+  });
+
+  it("locks and withdraws NFT", async () => {
+    const newOwner = await Account.with({
+      client,
+      signer: { type: "None" },
     });
+    const lockTime = 3600; // Lock for 1 hour
+
+    // Lock NFT
+    const lockResult = await callThroughMultisig(owner, task4.methods.lockNFT(newOwner.address, lockTime), {
+      abi: Task4.abi,
+    });
+    expect(lockResult.decoded?.time).to.equal(lockTime);
+
+    // Try to withdraw NFT before lock time expires
+    try {
+      await callThroughMultisig(newOwner, task4.methods.withdrawNFT(), { abi: Task4.abi });
+    } catch (error) {
+      expect(error.error).to.equal("NFT is still locked");
+    }
+
+    // Wait for lock time to expire
+    await new Promise((resolve) => setTimeout(resolve, lockTime * 1000));
+
+    // Withdraw NFT
+    await callThroughMultisig(newOwner, task4.methods.withdrawNFT(), { abi: Task4.abi });
+    expect(await newOwner.getAddress()).to.equal(await task4.call({ method: "owner" }));
+  });
 });
-
-
